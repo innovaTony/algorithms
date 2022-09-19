b1e9b9581ec5b61c6018f1504e635b18ba32b14d
innovaTony <antoinnaoun@gmail.com>
2022-09-19 23:30:42 +0300

    Add a sample 2D array

--- src/main/kotlin/algo1/Algo1.kt
+++ src/main/kotlin/algo1/Algo1.kt
@@ -3,6 +3,12 @@ package algo1
 
 var seatId = 0
 
+val twoDimensionalArray = arrayOf(
+    arrayOf(1, 0, 1),
+    arrayOf(0, 1, 1),
+    arrayOf(1, 1, 0),
+    arrayOf(0, 1, 1)
+)
 data class Seat  ( //Each element equal to 1 inside the 2D array will be considered as a Seat for 1 and hence the naming
     var id: Int,
     var x : Int,
