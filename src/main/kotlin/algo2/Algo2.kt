6ed8ed4f5882be408eb3a06e1190b92e7c9b06f7
innovaTony <antoinnaoun@gmail.com>
2022-10-08 19:06:27 +0300

    Refactor Algo2.kt

--- src/main/kotlin/algo2/Algo2.kt
+++ src/main/kotlin/algo2/Algo2.kt
@@ -8,53 +8,75 @@ The Author states in the end of the vid about a challenge:
         Which is to prevent looping back to the same element you came from
 
 */
-import algo2.LettersAlgo.dictionary
-import algo2.LettersAlgo.directions
-import algo2.LettersAlgo.rectangle
 import algo2.LettersAlgo.isWordUsed
 
 object LettersAlgo{
+    val isWordUsed = mutableListOf<Int>()
+}
 
-    const val rectangleRaw = """
+fun main(){
+     val rectangleRaw = """
     KOTE
     NULE
     AFIN
 """
 
-    const val dictionaryRaw = "Kotlin, fun, file, line, null"
+    val dictionaryRaw = "Kotlin, fun, file, line, null"
     val directions = listOf(1 to 0, 0 to 1, -1 to 0, 0 to -1)
 
-    val rectangle = LetterRectangleAlgo().convertStringToListOfStrings(rectangleRaw,"\n")
-    val dictionary = dictionaryRaw.toUpperCase().split(", ").toSet()
-    val isWordUsed = mutableListOf<Int>()
-}
 
-
-
-fun constructList(): MutableList<Int>{ //Form a list dynamically, and set the initial value of each index to 0, which will be reassigned to 1 for used words
-    for (i in dictionary){
-        isWordUsed.add(0)
-    }
-    return isWordUsed
-}
-
-fun main(){
-    constructList()
-
-    //Join existing words from the given to a set
-    val prefixes =  dictionary.flatMap { word ->
-        List(word.length +1){word.take(it)}
-    }.toSet()
-
-    for (y in rectangle.indices) for (x in rectangle[0].indices){
-        LetterRectangleAlgo().search(x= x,y= y, directions =  directions, word = rectangle[y][x].toString(), actualLettersToBeFetched = rectangle, comparedItems =  dictionary, prefixes = prefixes,1 to 0)
-    }
+    LetterRectangleAlgo().runLettersInDictionaryAlgo(rectangleRaw, dictionaryRaw, directions)
 
 }
+
 class LetterRectangleAlgo {
 
-    fun convertStringToListOfStrings(value:String, delimiter:String) : List<String>{
+    fun runLettersInDictionaryAlgo(
+        rectangleRaw: String,
+        dictionaryRaw: String,
+        directions: List<Pair<Int, Int>>
+    ) {
+        val rectangle = LetterRectangleAlgo().convertStringToListOfStrings(rectangleRaw, "\n")
+        val dictionary = dictionaryRaw.toUpperCase().split(", ").toSet()
+
+
+        LetterRectangleAlgo().constructList(dictionary)
+
+        //Join existing words from the given to a set
+        val prefixes = dictionary.flatMap { word ->
+            List(word.length + 1) { word.take(it) }
+        }.toSet()
+
+        for (y in rectangle.indices) for (x in rectangle[0].indices) {
+            LetterRectangleAlgo().search(
+                x = x,
+                y = y,
+                directions = directions,
+                word = rectangle[y][x].toString(),
+                actualLettersToBeFetched = rectangle,
+                comparedItems = dictionary,
+                prefixes = prefixes,
+                1 to 0
+            )
+        }
+    }
+
+
+
+
+
+
+
+
+    private fun convertStringToListOfStrings(value:String, delimiter:String) : List<String>{
         return value.trimIndent().split(delimiter)
+    }
+
+     private fun constructList(dictionary:Set<String>): MutableList<Int>{ //Form a list dynamically, and set the initial value of each index to 0, which will be reassigned to 1 for used words
+        for (i in dictionary){
+            isWordUsed.add(0)
+        }
+        return isWordUsed
     }
 
     private fun convertDirectionToOpposite(position: Pair<Int,Int>):Pair<Int,Int>{
@@ -67,7 +89,7 @@ class LetterRectangleAlgo {
         return  toBeMultiplied
     }
 
-    fun search(x:Int,y:Int,directions : List<Pair<Int,Int>>,word: String, actualLettersToBeFetched: List<String>, comparedItems: Set<String>, prefixes: Set<String>, position: Pair<Int,Int> = 0 to 0 ){
+   private fun search(x:Int,y:Int,directions : List<Pair<Int,Int>>,word: String, actualLettersToBeFetched: List<String>, comparedItems: Set<String>, prefixes: Set<String>, position: Pair<Int,Int> = 0 to 0 ){
 
          if (word !in prefixes ) return
 
@@ -88,13 +110,13 @@ class LetterRectangleAlgo {
 
         }
     }
-    private fun setWordUsed(word: String){ //Iterate through the list and assign the 1 value for used words, keep 0 otherwise
+    private fun setWordUsed(word: String,dictionary:Set<String>){ //Iterate through the list and assign the 1 value for used words, keep 0 otherwise
         dictionary.forEach {
             if (it == word) isWordUsed[dictionary.indexOf(it)] = 1
         }
     }
 
-    private fun isWordUsed(word: String) : Boolean{ //Check if word is used
+    private fun isWordUsed(word: String,dictionary:Set<String>) : Boolean{ //Check if word is used
         var check = false
         dictionary.forEach {
             if (it == word) check = isWordUsed[dictionary.indexOf(it)] == 1
@@ -112,9 +134,9 @@ class LetterRectangleAlgo {
         }   else {
             word
         }
-        if (checkedWord in comparedItems && !isWordUsed(checkedWord)) { //Both checks shall conform: word present in current given dictionary, and it shall not be used, so it won't fire several times
+        if (checkedWord in comparedItems && !isWordUsed(checkedWord,comparedItems)) { //Both checks shall conform: word present in current given dictionary, and it shall not be used, so it won't fire several times
             println(checkedWord)
-            setWordUsed(checkedWord)
+            setWordUsed(checkedWord,comparedItems)
             return true
         }
         return false
